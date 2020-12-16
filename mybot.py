708fe4378768465b7f05e997e81509be3fe02957
Abdul Wassay <wassaysardar35@gmail.com>
2020-12-16 22:18:39 +0500

    Update mybot.py
    Define FindClass and JoinClass method in MyBot class to find class
    in class list and Join the class

--- mybot.py
+++ mybot.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 import time
 import sys
+import keyboard
 
 class MyBot:
-    # def __init__(self, credentials):
-    #     self.CREDENTIALS = credentials
-    #     self.bot = webdriver.Chrome('chromedriver.exe')
-    #     self.bot.get('https://teams.microsoft.com')
-    #     time.sleep(5)
+    def __init__(self, credentials):
+        self.CREDENTIALS = credentials
+        self.bot = webdriver.Chrome('chromedriver.exe')
+        self.bot.get('https://teams.microsoft.com')
+        time.sleep(5)
 
     def Login(self):
         # Inputting email in input box
@@ -31,11 +32,14 @@ class MyBot:
         # Refusing Stay Signed In option
         btn_no = self.bot.find_element_by_xpath('//*[@id="idBtn_Back"]')
         btn_no.click()
-        time.sleep(5)
+        time.sleep(10)
 
         # Clicking Use Web App instead option
-        btn_useWebApp = self.bot.find_element_by_xpath('//*[@id="download-desktop-page"]/div/a')
-        btn_useWebApp.click()
+        try:
+            btn_useWebApp = self.bot.find_element_by_xpath('//*[@id="download-desktop-page"]/div/a')
+            btn_useWebApp.click()
+        except:   
+            time.sleep(15)
 
     def getClassSchedule(self):
         try:
@@ -46,7 +50,46 @@ class MyBot:
         except Exception as e:
             print(e)
             sys.exit(1)
-            
+
+    def FindClass(self, class_name):
+        available_teams = self.bot.find_elements_by_class_name('name-channel-type')
+        
+        for team in available_teams:
+            if class_name.upper() in team.get_attribute('innerHTML').upper():
+                team.click()
+
+
+    def JoinClass(self):
+        self.FindClass('bot testing')
+        time.sleep(15)
+        # Finding and Clicking Join button
+        btn_join = self.bot.find_element_by_xpath('//*[@id="m1608134726965"]/calling-join-button/button')
+        btn_join.click()
+        time.sleep(2)
+
+        # Blocking Camera and Mic
+        for _ in range(2):
+            keyboard.send('tab', do_press=True, do_release=True)
+            keyboard.send('tab', do_press=True, do_release=True)
+            keyboard.send('tab', do_press=True, do_release=True)
+            keyboard.send('enter', do_press=True, do_release=True)
+            time.sleep(2)
+
+        # Continue without audio
+        keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('enter', do_press=True, do_release=True)
+        time.sleep(2)
+
+        # Join 
+        keyboard.send('enter', do_press=True, do_release=True)
+        print('Class Joined')
+
+        # Leave Class
+        time.sleep(20)
+        for _ in range(7):
+            keyboard.send('tab', do_press=True, do_release=True)
+        keyboard.send('enter', do_press=True, do_release=True)
+
 
 
 credentials = {'email':'', 'password':''}
@@ -58,7 +101,6 @@ with open('credentials.txt') as f:
     credentials['password'] = content_lst[1]
 
 if __name__ == '__main__':
-    # bot = MyBot(credentials)
-    bot = MyBot()
-    bot.getClassSchedule()
-    # bot.Login()
+    bot = MyBot(credentials)
+    bot.Login()
+    bot.JoinClass()
